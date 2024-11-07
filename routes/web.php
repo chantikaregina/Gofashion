71674ac7d0a675a7ff9ef40f02204435b0cf589e
chantikaregina <reginachantika50@gmail.com>
2024-11-07 15:30:11 +0700

    membuat login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\UserLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [UserLoginController::class, 'login'])->name('login');
+Route::post('/login', [UserLoginController::class, 'auth'])->name('auth');
