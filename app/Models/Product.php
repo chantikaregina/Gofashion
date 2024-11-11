c92f2db6b880d69da73c4aeec19f2490f24a131f
chantikaregina <reginachantika50@gmail.com>
2024-11-11 13:23:16 +0700

    membuat login

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'product';
+    protected $primaryKey = 'id_product';
+
+    protected $fillable = [
+        'id_category',
+        'nama_product',
+        'deskripsi',
+        'harga_product',
+        'stock_product',
+    ];
+}
