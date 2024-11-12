e584d53b502d9d13f5a0a16d7cd622b15021c174
olivaazarroh <azarroholiva@gmail.com>
2024-11-12 09:32:58 +0700

    membuat halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\UserLoginController;
+use App\Http\Controllers\Backend\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +12,5 @@ Route::middleware(['guest'])->group(function () {
     Route::get('/login', [UserLoginController::class, 'login'])->name('login');
     Route::post('/login', [UserLoginController ::class, 'auth'])->name('auth');
 });
+
+Route::get('/admin/dashboard', [UserController::class, 'dashboard'])->name('admin.dashboard');
