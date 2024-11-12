0b40f55ef12db70f3412018cbff2343cd20bfdee
rastiii2 <rastianggraini62@smp.belajar.id>
2024-11-12 11:43:21 +0700

    membuat frontend

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,13 @@
 
 use App\Http\Controllers\Auth\UserLoginController;
 use App\Http\Controllers\Backend\UserController;
-use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/login', [UserLoginController::class, 'login'])->name('login');
     Route::post('/login', [UserLoginController ::class, 'auth'])->name('auth');
 });
-
-Route::get('/admin/dashboard', [UserController::class, 'dashboard'])->name('admin.dashboard');
-
-Route::get('/', [HomeController::class, 'home'])->name('home');
