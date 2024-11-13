bc32aad11362abb02ac7f72ee3bbfe14083cdaa1
rastiii2 <rastianggraini62@smp.belajar.id>
2024-11-13 16:21:17 +0700

    membuat crud category

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -3,9 +3,70 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function category()
+    {
+        $categorys = Category::all();
+        return view('backend.admin.category', compact('categorys'));
+    }
+
+    public function create()
+    {
+        return view('backend.admin.category_tambah');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_category' => 'required',
+            'deskripsi' => 'required',
+        ]);
+
+        Category::create([
+            'nama_category' => $request->nama_category,
+            'deskripsi' => $request->deskripsi,
+        ]);
+
+        return redirect()->route('admin.category')->with('success', 'Category Berhasil di Tambah.');
+    }
+
+    public function edit(string $id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return back();
+        }
+        return view('backend.admin.category_edit', compact('category'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+
+        $category = Category::find($id);
+
+        $request->validate([
+            'nama_category' => 'required',
+            'deskripsi' => 'required',
+        ]);
+
+        $category->update([
+            'nama_category' => $request->nama_category,
+            'deskripsi' => $request->deskripsi,
+        ]);
+
+        return redirect()->route('admin.category')->with('succes', 'Category Berhasil di Update.');
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        $category->delete();
+
+        return redirect()->route('admin.category')->with('success', 'Category Berhasil di Hapus.');
+    }
 }
