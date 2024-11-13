9eae818c1b361cd8eac2cf008195a82b66439360
chantikaregina <reginachantika50@gmail.com>
2024-11-13 16:25:58 +0700

    hapus

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\UserLoginController;
+use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\UserController;
 use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,7 @@ Route::middleware(['user'])->group(function () {
     Route::get('/admin/dashboard', [UserController::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [UserController::class, 'logout'])->name('admin.logout');
     Route::get('/admin/profile', [UserController::class, 'profile'])->name('admin.profile');
+    Route::get('/admin/product', [ProductController::class, 'product'])->name('admin.product');
 });
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
