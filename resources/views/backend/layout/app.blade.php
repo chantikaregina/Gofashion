879ad346d590485e72644e36b011b1a6590f8450
iqblhnf <iqbalhanif30052004@gmail.com>
2024-12-02 09:45:15 +0700

    perbaikan layouts backend

--- resources/views/backend/layout/app.blade.php
+++ resources/views/backend/layout/app.blade.php
@@ -6,11 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>GOFASHION - @yield('title')</title>
   <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/seodashlogo.png') }}" />
+  <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
   <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
-
-  <!-- Icon Font Stylesheet -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
 </head>
 
 <body>
@@ -22,8 +20,8 @@
       <!-- Sidebar scroll-->
       <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="" class="text-nowrap logo-img">
-            <h1 style="color: #00C0F0; font-weight: 700;">GOFASHION</h1>
+          <a href="./index.html" class="text-nowrap logo-img">
+            <img src="../assets/images/logos/logo-light.svg" alt="" />
           </a>
           <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
             <i class="ti ti-x fs-8"></i>
@@ -80,8 +78,9 @@
                 <span class="hide-menu">Setting</span>
               </a>
             </li>
-
-            <!-- End Sidebar navigation -->
+          </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
       </div>
       <!-- End Sidebar scroll-->
     </aside>
@@ -97,7 +96,12 @@
                 <i class="ti ti-menu-2"></i>
               </a>
             </li>
-
+            <li class="nav-item">
+              <a class="nav-link nav-icon-hover" href="javascript:void(0)">
+                <i class="ti ti-bell-ringing"></i>
+                <div class="notification bg-primary rounded-circle"></div>
+              </a>
+            </li>
           </ul>
           <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
             <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
@@ -113,132 +117,34 @@
                       <i class="ti ti-user fs-6"></i>
                       <p class="mb-0 fs-3">My Profile</p>
                     </a>
-                    <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-                      <i class="ti ti-x fs-8"></i>
-                    </div>
-                  </div>
-                  <!-- Sidebar navigation-->
-                  <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-                    <ul id="sidebarnav">
-                      <li class="nav-small-cap">
-                        <i class="ti ti-dots nav-small-cap-icon fs-6"></i>
-                        <span class="hide-menu">Home</span>
-                      </li>
-                      <li class="sidebar-item">
-                        <a class="sidebar-link" href="{{ route('dashboard') }}" aria-expanded="false">
-                          <span>
-                            <i class="ti ti-dashboard"></i>
-                          </span>
-                          <span class="hide-menu">Dashboard</span>
-                        </a>
-                      </li>
-                      <li class="nav-small-cap">
-                        <i class="ti ti-dots nav-small-cap-icon fs-6"></i>
-                        <span class="hide-menu">MENU</span>
-                      </li>
-                      <li class="sidebar-item">
-                        <a class="sidebar-link" href="{{ route('category') }}" aria-expanded="false">
-                          <span>
-                            <i class="ti ti-category"></i>
-                          </span>
-                          <span class="hide-menu">Category</span>
-                        </a>
-                      </li>
-                      <li class="sidebar-item">
-                        <a class="sidebar-link" href="{{ route('product') }}" aria-expanded="false">
-                          <span>
-                            <i class="ti ti-brand-producthunt"></i>
-                          </span>
-                          <span class="hide-menu">Produk</span>
-                        </a>
-                      </li>
-                      <li class="sidebar-item">
-                        <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
-                          <span>
-                            <i class="ti ti-brand-onedrive"></i>
-                          </span>
-                          <span class="hide-menu">Review</span>
-                        </a>
-                      </li>
-                      <li class="sidebar-item">
-                        <a class="sidebar-link" href="{{ route('setting') }}" aria-expanded="false">
-                          <span>
-                            <i class="ti ti-settings"></i>
-                          </span>
-                          <span class="hide-menu">Setting</span>
-                        </a>
-                      </li>
-
-                      <!-- End Sidebar navigation -->
-                </div>
-                <!-- End Sidebar scroll-->
-                </aside>
-                <!--  Sidebar End -->
-                <!--  Main wrapper -->
-                <div class="body-wrapper">
-                  <!--  Header Start -->
-                  <header class="app-header">
-                    <nav class="navbar navbar-expand-lg navbar-light">
-                      <ul class="navbar-nav">
-                        <li class="nav-item d-block d-xl-none">
-                          <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse"
-                            href="javascript:void(0)">
-                            <i class="ti ti-menu-2"></i>
-                          </a>
-                        </li>
-
-                      </ul>
-                      <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-                        <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-
-                          <li class="nav-item dropdown">
-                            <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
-                              data-bs-toggle="dropdown" aria-expanded="false">
-                              <img src="../assets/images/profile/user-1.jpg" alt="" width="35"
-                                height="35" class="rounded-circle">
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
-                              aria-labelledby="drop2">
-                              <div class="message-body">
-                                <a href="{{ route('profile') }}"
-                                  class="d-flex align-items-center gap-2 dropdown-item">
-                                  <i class="ti ti-user fs-6"></i>
-                                  <p class="mb-0 fs-3">My Profile</p>
-                                </a>
-                                <a href="{{ route('logout') }}"
-                                  class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-                              </div>
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                    </nav>
-                  </header>
-                  <!--  Header End -->
-                  <div class="container-fluid">
-                    @yield('content')
-
-                    <div class="py-6 px-6 text-center">
-                      <p class="mb-0 fs-4">Design and Developed by <a href="https://adminmart.com/" target="_blank"
-                          class="pe-1 text-primary text-decoration-underline">AdminMart.com</a>Distributed by <a
-                          href="https://themewagon.com/" target="_blank"
-                          class="pe-1 text-primary text-decoration-underline">ThemeWagon</a></p>
-                    </div>
+                    <a href="{{ route('logout') }}" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
                   </div>
                 </div>
+              </li>
+            </ul>
           </div>
-          <script src="{{ asset('../assets/libs/jquery/dist/jquery.min.js') }}"></script>
-          <script src="{{ asset('../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-          <script src="{{ asset('../assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-          <script src="{{ asset('../assets/libs/simplebar/dist/simplebar.js') }}"></script>
-          <script src="{{ asset('../assets/js/sidebarmenu.js') }}"></script>
-          <script src="{{ asset('../assets/js/app.min.js') }}"></script>
-          <script src="{{ asset('../assets/js/dashboard.js') }}"></script>
-          <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
-
-
-          <script src=https://cdn.datatables.net/2.1.8/js/dataTables.js></script>
-          <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+        </nav>
+      </header>
+      <!--  Header End -->
+      <div class="container-fluid">
+        @yield('content')
+        <div class="py-6 px-6 text-center">
+          <p class="mb-0 fs-4">Design and Developed by <a href="https://adminmart.com/" target="_blank"
+              class="pe-1 text-primary text-decoration-underline">AdminMart.com</a>Distributed by <a href="https://themewagon.com/" target="_blank"
+              class="pe-1 text-primary text-decoration-underline">ThemeWagon</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+  <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+  <script src="{{ asset('assets/js/app.min.js') }}"></script>
+  <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+  <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
 </body>
 
 </html>
